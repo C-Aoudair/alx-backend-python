65e4c6a5c97468f2a41edae82bdddd09f0e86431
C-Aoudair <aoudair16031999@gmail.com>
2024-06-06 18:41:50 +0100

    add task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This module contains a function that takes two arguments
+    and returns their concatenated strings. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This function takes two arguments and returns their
+        concatenated strings. """
+    return (str1 + str2)

0f2f8af5b0c89d91c4a9389f65456304f4ebeef4
C-Aoudair <aoudair16031999@gmail.com>
2024-06-06 18:36:36 +0100

    add task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module contains a function that takes two arguments
+    and returns their sum. """
+
+
+def add(a: float, b: float) -> float:
+    """ This function takes two arguments and returns their sum. """
+    return (a + b)

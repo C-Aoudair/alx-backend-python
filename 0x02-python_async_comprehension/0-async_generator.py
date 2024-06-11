9d7b72ae87f1ffa90bfede27812287529831c632
C-Aoudair <aoudair16031999@gmail.com>
2024-06-11 15:51:00 +0100

    test annotation for async function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Contains async_generator coroutine """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Generates 10 random numbers between 0 and 10 """
     for _ in range(10):
         await asyncio.sleep(1)

820baf21ab64c058a25851c33933d3ad9320b76b
C-Aoudair <aoudair16031999@gmail.com>
2024-06-11 18:57:22 +0100

    add task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Contains async_comprehension coroutine """
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Collects 10 random numbers using an async generator """
+    return [randomValue async for randomValue in async_generator()]

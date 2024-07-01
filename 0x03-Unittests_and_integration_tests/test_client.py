373284548633fa966c6d43e81daffeec8cc9da8c
C-Aoudair <aoudair16031999@gmail.com>
2024-07-01 17:00:38 +0100

    update test_client.py: add unit test for client.GithubOrgClient._puplic_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ Unit tests for client module units."""
 
 from client import GithubOrgClient
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 
 import unittest
@@ -21,3 +21,12 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, mock_response)
         url = f"https://api.github.com/orgs/{org_name}"
         mock_get_json.assert_called_once_with(url)
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_puplic_repos_url(self, mock_property):
+        mock_response = {"repos_url": "example.com"}
+        mock_property.return_value = mock_response
+        result = GithubOrgClient("org_name")._public_repos_url
+
+        self.assertEqual(result, mock_response["repos_url"])
+        mock_property.assert_called_once()

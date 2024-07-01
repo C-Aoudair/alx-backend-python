2ca31f70b7d0ffe69bb51c7c55b1e709d5e77212
C-Aoudair <aoudair16031999@gmail.com>
2024-07-01 01:05:42 +0100

    update test_utils.py: add test_access_nested_map_exception method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,12 +9,22 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     """Implement unit tests for test_access_nested_map funtion"""
 
-    @parameterized.expand([
-        ("case1", {"a": 1}, ("a",), 1),
-        ("case2", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("case3", {"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-
+    @parameterized.expand(
+        [
+            ("case1", {"a": 1}, ("a",), 1),
+            ("case2", {"a": {"b": 2}}, ("a",), {"b": 2}),
+            ("case3", {"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(self, name, a, b, expected):
         self.assertEqual(access_nested_map(a, b), expected)
 
+    @parameterized.expand(
+        [
+            ("case1", {}, ("a",)),
+            ("case2", {"a": 1}, ("a", "b")),
+        ]
+    )
+    def test_access_nested_map_exception(self, name, a, b):
+        with self.assertRaises(KeyError):
+            access_nested_map(a, b)
